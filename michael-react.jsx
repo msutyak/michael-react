6833306b027a72bf96040bc559ebab6c19f69673
Michael Sutyak <msutyak@gmail.com>
2016-06-08 00:02:13 -0700

    added force-ssl

--- michael-react.jsx
+++ michael-react.jsx
@@ -1,8 +1,9 @@
 if (Meteor.isClient) {
 
   Meteor.startup(function () {
+
     // Use Meteor.startup to render the component after the page is ready
     ReactDOM.render(<App />, document.getElementById("render-target"));
-
+    
   });
 }
