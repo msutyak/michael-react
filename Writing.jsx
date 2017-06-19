fec0ca93e9e15c44f4bc16f9bd499bf39287eafd
Michael Sutyak <msutyak@gmail.com>
2017-06-18 20:42:37 -0700

    change writing

--- Writing.jsx
+++ Writing.jsx
@@ -12,10 +12,10 @@ Writing = React.createClass({
 						<h2>Writing & Film</h2>
 						<h3>TV, Film, Etc...</h3>
 					</header>
-					<p><a href="/writing/backpackers.pdf" target="_blank">"Backpackers" Pilot Script</a></p>
-					<p><a href="/writing/BackpackersSHOWBIBLE.pdf" target="_blank">"Backpackers" Show Bible</a></p>
+					<p><a href="/images/backpackers_1.jpg" target="_blank">"Backpackers" Promo (Pilot Script Upon Request)</a></p>
+					<p><a href="mailto:msutyak@gmail.com" target="_blank">"Backpackers" Show Bible (Upon Request)</a></p>
 					<p><a href="/writing/siliconvalleys04e01dosed.pdf" target="_blank">"Silicon Valley" Spec Script - S04E01 "Dosed"</a></p>
-					<p><a href="/writing/sexcom.pdf" target="_blank">"Sex.com: The Crown Jewel" Feature Script</a></p>
+					<p><a href="mailto:msutyak@gmail.com" target="_blank">"Sex.com: The Crown Jewel" Feature Script (Upon Request)</a></p>
 					<p><a href="https://vimeo.com/123588299" target="_blank">"The Line" Short Film</a></p>
 				</div>
 			</section>	
